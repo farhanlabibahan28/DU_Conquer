91a51e9daae81e856367fb9ce964fdb4597f1f3b
Farhan Labib Ahan <farhanlabibahan@Ahans-MacBook.local>
2025-05-22 06:49:59 +0600

    Code fix

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,16 +1,17 @@
-// main.c
-#include "raylib.h"
+#include <raylib.h>
+
 
 int main(void) {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "Hello Raylib");
+   
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+        
         EndDrawing();
     }
 
