00815e475acb205b4164f825f2b37f1f8b784cea
Farhan Labib Ahan <farhanlabibahan@gmail.com>
2025-06-14 21:06:30 +0600

    Menu

--- MINI GAMES/menu/main.cpp
+++ MINI GAMES/menu/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 Color red = {255, 0, 0, 255};
 Color blackTransparent = {0, 0, 0, 128};
+Vector2 cloudPos = {0,0};
+
+Sound clickSound;
+Music bgmMenu;
+bool wasHoveredNew = false, wasHoveredHigh = false, wasHoveredHelp = false, wasHoveredMap = false, wasHoveredSaved = false, wasHoveredExit = false;
 
 class Button {
 public:
@@ -16,7 +21,7 @@ public:
         : bounds{ x, y, width, height },
           label(text),
           textColorNormal(WHITE),
-          textColorHover(RED) {}
+          textColorHover(MAGENTA) {}
 
     bool IsHovered() const {
         return CheckCollisionPointRec(GetMousePosition(), bounds);
@@ -39,23 +44,36 @@ public:
 };
 
 int main() {
+    
+
+    InitWindow(GetMonitorWidth(0), GetMonitorHeight(0), "DU_Conquer_Menu");
+    ToggleFullscreen();
+    InitAudioDevice();
+    clickSound = LoadSound("resources/click.mp3");
+    bgmMenu = LoadMusicStream("resources/bgm_menu.mp3");
+    SetMusicVolume(bgmMenu, 0.2f);
+    PlayMusicStream(bgmMenu);
+
     const int screenWidth = GetMonitorWidth(0);
     const int screenHeight = GetMonitorHeight(0);
 
-    InitWindow(screenWidth, screenHeight, "DU_Conquer");
     Texture2D main_menu = LoadTexture("resources/main_menu.png");
+    Texture2D cloud = LoadTexture("resources/cloud.png");
+    float scale = (float)GetMonitorHeight(0) / main_menu.height;
 
-    int buttonWidth = 250;
-    int buttonHeight = 40;
-    int spacing = 40;
-    int startY = 400;
+    int buttonWidth = 250 * scale;
+    int buttonHeight = 40 * scale;
+    int spacing = 40 * scale;
+    int startY = GetMonitorHeight(0)-150;
+    int startX = GetMonitorWidth(0)-330;
+    
 
-    Button btnNew(screenWidth, startY + spacing * 0, buttonWidth, buttonHeight, "New Game");
-    Button btnHigh(screenWidth, startY + spacing * 1, buttonWidth, buttonHeight, "High Score");
-    Button btnHelp(screenWidth, startY + spacing * 2, buttonWidth, buttonHeight, "Help");
-    Button btnMap(screenWidth, startY + spacing * 3, buttonWidth, buttonHeight, "Map");
-    Button btnSaved(screenWidth, startY + spacing * 4, buttonWidth, buttonHeight, "Saved Games");
-    Button btnExit(screenWidth, startY + spacing * 5, buttonWidth, buttonHeight, "Exit");
+    Button btnNew(startX, startY - spacing * 6, buttonWidth, buttonHeight, "New Game");
+    Button btnHigh(startX, startY - spacing * 5, buttonWidth, buttonHeight, "High Score");
+    Button btnHelp(startX, startY - spacing * 4, buttonWidth, buttonHeight, "Help");
+    Button btnMap(startX, startY - spacing * 3, buttonWidth, buttonHeight, "Map");
+    Button btnSaved(startX, startY - spacing * 2, buttonWidth, buttonHeight, "Saved Games");
+    Button btnExit(startX, startY - spacing * 1, buttonWidth, buttonHeight, "Exit");
 
     const char* clickedMessage = nullptr;
     bool high_score_b = false;
@@ -64,38 +82,36 @@ int main() {
 
     while (!WindowShouldClose()) {
         // Dialog Box setup
-        int boxWidth = 500;
-        int boxHeight = 300;
-        int x = screenWidth / 2 - boxWidth / 2 + 100;
-        int y = screenHeight / 2 - boxHeight / 2 + 200;
+        int boxWidth = 500*scale;
+        int boxHeight = 300*scale;
+        int x = screenWidth / 2 - boxWidth / 2;
+        int y = screenHeight - 350*scale ;
         Rectangle closeBtn = { x + boxWidth - 35, y + 10, 25, 25 };
 
         // Mouse Clicks
-        if (btnNew.IsClicked()) {
-            clickedMessage = "New Game clicked!";
-        }
+        if (btnNew.IsHovered() && !wasHoveredNew) PlaySound(clickSound);
+        wasHoveredNew = btnNew.IsHovered();
+        if (btnNew.IsClicked()) { PlaySound(clickSound); clickedMessage = "New Game clicked!"; }
 
-        if (btnHigh.IsClicked()) {
-            clickedMessage = "High Scores";
-            high_score_b = true;
-        }
+        if (btnHigh.IsHovered() && !wasHoveredHigh) PlaySound(clickSound);
+        wasHoveredHigh = btnHigh.IsHovered();
+        if (btnHigh.IsClicked()) { PlaySound(clickSound); clickedMessage = "High Scores"; high_score_b = true; }
 
-        if (btnHelp.IsClicked()) {
-            clickedMessage = "Help clicked!";
-        }
+        if (btnHelp.IsHovered() && !wasHoveredHelp) PlaySound(clickSound);
+        wasHoveredHelp = btnHelp.IsHovered();
+        if (btnHelp.IsClicked()) { PlaySound(clickSound); clickedMessage = "Help clicked!"; }
 
-        if (btnMap.IsClicked()) {
-            clickedMessage = "Map clicked!";
-        }
+        if (btnMap.IsHovered() && !wasHoveredMap) PlaySound(clickSound);
+        wasHoveredMap = btnMap.IsHovered();
+        if (btnMap.IsClicked()) { PlaySound(clickSound); clickedMessage = "Map clicked!"; }
 
-        if (btnSaved.IsClicked()) {
-            clickedMessage = "Saved Games clicked!";
-        }
+        if (btnSaved.IsHovered() && !wasHoveredSaved) PlaySound(clickSound);
+        wasHoveredSaved = btnSaved.IsHovered();
+        if (btnSaved.IsClicked()) { PlaySound(clickSound); clickedMessage = "Saved Games clicked!"; }
 
-        if (btnExit.IsClicked()) {
-            CloseWindow();
-            return 0;
-        }
+        if (btnExit.IsHovered() && !wasHoveredExit) PlaySound(clickSound);
+        wasHoveredExit = btnExit.IsHovered();
+        if (btnExit.IsClicked()) { PlaySound(clickSound); CloseWindow(); return 0; }
 
         // Box Close
         if (high_score_b &&
@@ -105,21 +121,36 @@ int main() {
             clickedMessage = nullptr;
         }
 
+        cloudPos.y = -120*scale;
+        // Bidirectional cloud oscillation
+        static float cloudSpeed = 20.0f;
+        static bool movingRight = true;
+        if (movingRight) {
+            cloudPos.x += cloudSpeed * GetFrameTime();
+            if (cloudPos.x >= GetMonitorWidth(0) - cloud.width * scale)
+                movingRight = false;
+        } else {
+            cloudPos.x -= cloudSpeed * GetFrameTime();
+            if (cloudPos.x <= 0)
+                movingRight = true;
+        }
+
         // Drawing
         BeginDrawing();
         ClearBackground(BLACK);
 
-        DrawTexture(main_menu, 0, 0, WHITE);
-        DrawText("DU Conquer", 20, 320, 50, red);
+        DrawTextureEx(main_menu, (Vector2){0, 0}, 0.0f, scale, WHITE);
+        DrawTextureEx(cloud, cloudPos, 3.0f, 1.1 *scale, MAGENTA);
+        DrawText("DU Conquer", startX-15, startY - spacing * 8, 55 * scale, MAGENTA);
 
-        int fontSize = 24;
+        int fontSize = 27 * scale;
 
-        btnNew.DrawButton(fontSize, blackTransparent);
-        btnHigh.DrawButton(fontSize, blackTransparent);
-        btnHelp.DrawButton(fontSize, blackTransparent);
-        btnMap.DrawButton(fontSize, blackTransparent);
-        btnSaved.DrawButton(fontSize, blackTransparent);
-        btnExit.DrawButton(fontSize, blackTransparent);
+        btnNew.DrawButton(fontSize, BLANK);
+        btnHigh.DrawButton(fontSize, BLANK);
+        btnHelp.DrawButton(fontSize, BLANK);
+        btnMap.DrawButton(fontSize, BLANK);
+        btnSaved.DrawButton(fontSize, BLANK);
+        btnExit.DrawButton(fontSize, BLANK);
 
         if (high_score_b) {
             DrawRectangleRounded((Rectangle){x, y, boxWidth, boxHeight}, 0.3f, 10, Fade(BLACK, 0.75f));
@@ -136,24 +167,29 @@ int main() {
             while (file >> name >> score)
                 scores.emplace_back(name, score);
 
+            DrawText("High Scores", x + 20, y + 20, 28 * scale, MAGENTA);
+
             int yp = y + 80;
             for (auto& it : scores) {
                 string toShow = it.first + "    " + to_string(it.second);
-                if (it.second >= 1000) toShow += "  (DU Conqured)";
-                else if (it.second == 0) toShow += "  (Failed)";
+                Color score_color = WHITE;
+                if (it.second >= 1000) { toShow += "  (DU Conqured)"; score_color = GREEN;}
+                else if (it.second == 0) {toShow += "  (Failed)"; score_color = RED;}
 
-                Color col = (it.second >= 1000) ? GREEN : (it.second == 0 ? WHITE : ORANGE);
-                DrawText(toShow.c_str(), x + 20, yp, 20, col);
+                DrawText(toShow.c_str(), x + 20, yp, 24 * scale, score_color);
                 yp += 30;
             }
-
-            DrawText(clickedMessage, x + 20, y + 40, 20, RED);
         }
 
+        UpdateMusicStream(bgmMenu);
         EndDrawing();
+        // cout<<"Screen Width: "<<screenWidth<<endl;
     }
 
     UnloadTexture(main_menu);
+    UnloadSound(clickSound);
+    UnloadMusicStream(bgmMenu);
+    CloseAudioDevice();
     CloseWindow();
 
     return 0;
